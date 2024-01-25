55fc120e6cf3db012099f93bb4b3b1987e8660a8
tarvt <taravatmonsef@yahoo.com.sg>
2024-01-25 12:53:34 -0800

    add spme ports to allowed hosts

--- generator/project/project/settings.py
+++ generator/project/project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5mx7uzqs55hgr((3lmq-y#k)z2v*rv!ke(b2i&=f33zlod)x9b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.18.0.0']
+ALLOWED_HOSTS = ['0.0.0.0','172.18.0.0' , '127.0.0.1', 'localhost'] 
 
 # Application definition
 
